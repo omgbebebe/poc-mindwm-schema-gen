b0b183b20651c0aafe8916ad6f5f9bfb1ac744ef
Sergey Bubnov <omgbebebe@gmail.com>
2024-07-08 17:38:54 +0400

    initial

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.default = {
+          # basePackages = pkgs.haskellPackages;
+
+          # Packages to add on top of `basePackages`, e.g. from Hackage
+          packages = {
+#            aeson.source = "1.5.0.0";# Hackage version
+          };
+
+          # my-haskell-package development shell configuration
+          devShell = {
+            hlsCheck.enable = false;
+          };
+
+          # What should haskell-flake add to flake outputs?
+          autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "my-haskell-package custom development shell";
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+          ];
+          nativeBuildInputs = with pkgs; [
+            # other development tools.
+          ];
+        };
+        packages.default = self'.packages.asyncapi-gen;
+      };
+    };
+}
